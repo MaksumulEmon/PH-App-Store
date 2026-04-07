8ebb2f60efa631cf67459fbc0af95fb7f591a756
MaksumulEmon <maksumulemon300@gmail.com>
2026-04-07 11:38:12 +0600

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router/dom';
+import { createBrowserRouter } from 'react-router';
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+  {
+    path: "/apps",
+    element: <h1>All App Here</h1>
+  },
+  {
+    path: "/emon",
+    element: <h1>Emon Cst, 21 Age BD</h1>
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
